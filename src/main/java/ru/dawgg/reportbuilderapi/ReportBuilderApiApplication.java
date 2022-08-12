4b0ab1e6ac9075b17df735284e61c0ee7eeab556
Dawgg <l1kin3@mail.ru>
2022-08-12 14:17:47 +0300

    1

---
+++ src/main/java/ru/dawgg/reportbuilderapi/ReportBuilderApiApplication.java
@@ -0,0 +1,13 @@
+package ru.dawgg.reportbuilderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportBuilderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportBuilderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dawgg/reportbuilderapi/ReportBuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dawgg.reportbuilderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportBuilderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

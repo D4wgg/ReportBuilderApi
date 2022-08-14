94d0878155cecb0293981e45a21e41eb64ee5e9e
Dawgg <l1kin3@mail.ru>
2022-08-14 18:16:25 +0300

    added some validation

--- src/main/java/ru/dawgg/reportbuilderapi/controller/SingleQueryController.java
+++ src/main/java/ru/dawgg/reportbuilderapi/controller/SingleQueryController.java
@@ -1,12 +1,12 @@
 package ru.dawgg.reportbuilderapi.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.dawgg.reportbuilderapi.model.query.SingleQuery;
 import ru.dawgg.reportbuilderapi.service.SingleQueryService;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.*;
@@ -19,31 +19,31 @@ public class SingleQueryController {
     private final SingleQueryService queryService;
 
     @PostMapping("/add-new-query")
-    public ResponseEntity<Void> addNewQuery(@RequestBody SingleQuery singleQuery) {
+    public ResponseEntity<Void> addNewQuery(@RequestBody @NotNull SingleQuery singleQuery) {
         queryService.save(singleQuery);
         return new ResponseEntity<>(CREATED);
     }
 
     @PutMapping("/modify-query")
-    public ResponseEntity<Void> modifyQuery(@RequestBody SingleQuery singleQuery) {
+    public ResponseEntity<Void> modifyQuery(@RequestBody @NotNull SingleQuery singleQuery) {
         queryService.update(singleQuery);
         return new ResponseEntity<>(OK);
     }
 
     @DeleteMapping("/delete-single-query-by-id/{id}")
-    public ResponseEntity<Void> deleteSingleQueryById(@PathVariable("id") Integer id) {
+    public ResponseEntity<Void> deleteSingleQueryById(@PathVariable("id") @NotNull Integer id) {
         queryService.deleteById(id);
         return new ResponseEntity<>(ACCEPTED);
     }
 
     @GetMapping("/execute-single-query-by-id/{id}")
-    public ResponseEntity<Void> executeSingleQueryById(@PathVariable("id") Integer id) {
+    public ResponseEntity<Void> executeSingleQueryById(@PathVariable("id") @NotNull Integer id) {
         queryService.executeById(id);
         return new ResponseEntity<>(CREATED);
     }
 
     @GetMapping("/get-single-query-by-id/{id}")
-    public SingleQuery getSingleQueryById(@PathVariable("id") Integer id) {
+    public SingleQuery getSingleQueryById(@PathVariable("id") @NotNull Integer id) {
         return queryService.findById(id);
     }
 
@@ -51,6 +51,4 @@ public class SingleQueryController {
     public List<SingleQuery> getAllSingleQueries() {
         return queryService.findAll();
     }
-
-
 }
